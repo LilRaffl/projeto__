aab9217fd4c5bf9e886fa521eefe65aea38681ca
LilRaffl <caninosbrancoscb10@gmail.com>
2022-07-29 14:41:21 -0300

    novo

---
+++ app/src/main/java/com/example/projeto/menu_principal_layout.java
@@ -0,0 +1,14 @@
+package com.example.projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_principal_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_principal_layout);
+    }
+}
